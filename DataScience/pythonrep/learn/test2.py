0993f1d91e971d818963da74c70d9111e5b07f4d
richyu <richardminyu@foxmail.com>
2018-08-23 18:43:22 +0800

    add python test

--- DataScience/pythonrep/learn/test2.py
+++ DataScience/pythonrep/learn/test2.py
@@ -1,16 +1,19 @@
-print("input height")
+print("input height cm")
 height = input()
-print("input weight")
+print("input weight kg")
 weight = input()
+height = int(height)/100
+weight = int(weight)
 bmi = weight/(height*height)
-bmi = int(bmi)
+print(bmi)
+
 if bmi < 18.5:
     print("过轻")
-elif bmi >= 18.5 and bmi > 25:
+elif bmi >= 18.5 and bmi < 25:
     print("正常")
 elif bmi >= 25 and bmi < 28:
     print("过重")
 elif bmi >= 28 and bmi < 32:
     print("肥胖")
-elif bmi >= 32:
+else:
     print("严重肥胖")
