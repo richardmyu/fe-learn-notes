14f96c3682cce7ac1d812a65b94f806124577e03
richyu <richardminyu@foxmail.com>
2018-11-05 18:09:45 +0800

    Renormalization

--- FE/Node/koa2/REST/app.js
+++
@@ -1,9 +0,0 @@
-const Koa = require("koa");
-const bodyparser = require("koa-bodyparser");
-const controller = require("./controller");
-const app = new Koa();
-app.use(bodyparser);
-app.use(controller());
-
-app.listen(3333);
-console.log("app started at port 3000");
--- FE/Node/koa2/url/app5.js
+++
@@ -1,6 +0,0 @@
-const controller = require("./controller");
-const Koa = require("koa");
-const app = new Koa();
-app.use(controller);
-app.listen(3000);
-console.log("app started at port 3000");
