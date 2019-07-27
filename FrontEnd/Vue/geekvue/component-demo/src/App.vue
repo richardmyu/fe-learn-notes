0d5f852b59022cf4613c95084c32c4bcbee125c2
richyu <richardminyu@foxmail.com>
2019-07-27 14:42:01 +0800

    modify

---
+++ FrontEnd/Vue/geekvue/component-demo/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <learn-no-com></learn-no-com>
+    <learn-com></learn-com>
+  </div>
+</template>
+
+<script>
+import learnNoCom from "./components/learnNoCom";
+import learnCom from "./components/learnCom";
+
+export default {
+  name: "app",
+  components: {
+    "learn-no-com": learnNoCom,
+    "learn-com": learnCom
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ FrontEnd/Vue/geekvue/component-demo/src/components/learnNoCom.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+   <ul>
+     <li>
+       <input type="checkbox">
+       <span>学习VUE属性</span>
+       <button>删除</button>
+     </li>
+     <li>
+       <input type="checkbox">
+       <span>学习VUE事件</span>
+       <button>删除</button>
+     </li>
+     <li>
+       <input type="checkbox">
+       <span>学习VUE插槽</span>
+       <button>删除</button>
+     </li>
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "learnNoCom"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
