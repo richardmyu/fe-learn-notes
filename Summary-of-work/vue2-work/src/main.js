304746f1830f67bd6944f25d0b145aa0e57e4eba
richyu <richardminyu@foxmail.com>
2019-10-11 23:44:26 +0800

    test link

--- Summary-of-work/vue2-work/src/main.js
+++ Summary-of-work/vue2-work/src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import "element-ui/lib/theme-chalk/index.css";
 
 import Vue from "vue";
@@ -15,7 +13,6 @@ Vue.use(ElementUI, {
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
   el: "#app",
   router,
