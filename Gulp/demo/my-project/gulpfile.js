e293b9a9fa051e47f06bf6b185d883b1b4ff8ab1
yum <richardminyu@foxmail.com>
2021-01-07 20:45:30 +0800

    add gulp note

--- Gulp/demo/my-project/gulpfile.js
+++ Gulp/demo/my-project/gulpfile.js
@@ -1,6 +1,16 @@
-function defaultTask(cb) {
-  // console.log(cb);
+const { series, parallel } = require("gulp");
+
+function sayHello(cb) {
+  console.log('Hello world');
   cb();
 }
 
-exports.default = defaultTask;
+function sayBye(cb) {
+  console.log('bye bye');
+  cb();
+}
+
+exports.order = series(sayHello, sayBye);
+
+exports.concu = parallel(sayHello, sayBye);
+// exports.default = parallel(sayHello, sayBye);
