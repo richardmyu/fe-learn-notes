f5cf2f5b24043fd5d4555075bf09f389d1818e35
yum <richardminyu@foxmail.com>
2022-03-02 14:22:13 +0800

    update es5-02-03

--- Javascript(ES5)/JS02-语法/JS0203-操作符/index.js
+++ Javascript(ES5)/JS02-语法/JS0203-操作符/index.js
@@ -1,5 +1,19 @@
-let a = 3.14159;
-console.log(a);
-console.log(~a);
-console.log(~~a);
+let FLAG_A = 1; // 0001
+let FLAG_B = 2; // 0010
+let FLAG_C = 4; // 0100
+let FLAG_D = 8; // 1000
+
+let flags = 5;
+
+if (flags & FLAG_C) {
+  console.log('FLAG_C is opened');
+}
+
+let mask = FLAG_A | FLAG_B | FLAG_D;
+// 1011
+console.log(mask);
+
+flags = flags | mask;
+console.log(flags);
+
 
